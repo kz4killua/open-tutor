1658a0a13334e0ca52ffcb93107846aef0269c86
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-11 03:29:07 -0400

    Rename project to `config`

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@ sh /app/migrate.sh
 
 APP_PORT=${PORT:-8000}
 cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm opentutor.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm config.wsgi:application --bind "0.0.0.0:${APP_PORT}"
