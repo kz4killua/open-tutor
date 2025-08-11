c6cdc623e660bd66280b51cc544f9008db19a109
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-11 03:48:23 -0400

    Update project settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,11 +16,6 @@ from pathlib import Path
 
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
